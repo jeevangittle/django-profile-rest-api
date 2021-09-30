4b52cac2f3737919bbb125e8263876ebcde9f6b9
Jeevan <jeevan@changepay.co>
2021-09-30 20:57:56 +0530

    addes new models for profile feed

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
-
+from django.conf import settings
 
 
 
@@ -48,5 +48,17 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
   def __str__(self):
     """return string representation of our"""
     return self.email
+
+
+class ProfileFeedItem(models.Model):
+  """profile Status update feed"""
+  user_Profile = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+  status_text = models.CharField(max_length=255)
+  createdon = models.DateTimeField(auto_now_add=True)
+
+
+  def __str__(self):
+    """Return the model as a string """
+    return self.status_text
 
 
